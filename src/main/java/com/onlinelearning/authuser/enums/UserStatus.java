5b78a24183c05e89913bcc8b238114ea4f99a314
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-13 18:04:41 -0300

    create enums UserStatus and UserType

---
+++ src/main/java/com/onlinelearning/authuser/enums/UserStatus.java
@@ -0,0 +1,6 @@
+package com.onlinelearning.authuser.enums;
+
+public enum UserStatus {
+
+    ACTIVE, BLOCKED
+}
---
+++ src/main/java/com/onlinelearning/authuser/enums/UserType.java
@@ -0,0 +1,7 @@
+package com.onlinelearning.authuser.enums;
+
+public enum UserType {
+
+    ADMIN, STUDENT, INSTRUCTOR
+
+}
