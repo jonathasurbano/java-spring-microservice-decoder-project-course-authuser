f5d60d9c2c168dccdbcb79d1d8e159234269156a
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-13 18:07:08 -0300

    create repository UserRepository

---
+++ src/main/java/com/onlinelearning/authuser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.onlinelearning.authuser.repositories;
+
+import com.onlinelearning.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
