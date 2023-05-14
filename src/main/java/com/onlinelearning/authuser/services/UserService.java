945b8fff44c85159cf44b392db7c9563d9ce0b26
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-14 19:59:40 -0300

    create CRUD methods in UserService

--- src/main/java/com/onlinelearning/authuser/services/UserService.java
+++ src/main/java/com/onlinelearning/authuser/services/UserService.java
@@ -1,4 +1,21 @@
 package com.onlinelearning.authuser.services;
 
+import com.onlinelearning.authuser.models.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
+
+    void save(UserModel userModel);
+
+    boolean existsByUserName(String userName);
+
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/onlinelearning/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/onlinelearning/authuser/services/impl/UserServiceImpl.java
@@ -1,13 +1,48 @@
 package com.onlinelearning.authuser.services.impl;
 
+import com.onlinelearning.authuser.models.UserModel;
 import com.onlinelearning.authuser.repositories.UserRepository;
 import com.onlinelearning.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+
+    @Override
+    public void save(UserModel userModel) {
+        userRepository.save(userModel);
+    }
+
+    @Override
+    public boolean existsByUserName(String userName) {
+        return userRepository.existsByUserName(userName);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
