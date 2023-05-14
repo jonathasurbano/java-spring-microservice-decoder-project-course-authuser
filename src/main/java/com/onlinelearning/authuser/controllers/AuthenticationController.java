c8ccc2ceb0df4da19d765a0c8a47d831078d79f5
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-14 20:05:09 -0300

    create authentication controller

---
+++ src/main/java/com/onlinelearning/authuser/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.onlinelearning.authuser.controllers;
+
+import com.onlinelearning.authuser.dtos.UserDto;
+import com.onlinelearning.authuser.enums.UserStatus;
+import com.onlinelearning.authuser.enums.UserType;
+import com.onlinelearning.authuser.models.UserModel;
+import com.onlinelearning.authuser.services.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto) {
+        if (userService.existsByUserName(userDto.getUserName())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: UserName is already taken.");
+        }
+        if (userService.existsByEmail(userDto.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already taken.");
+        }
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        userModel.setUserStatus(UserStatus.ACTIVE);
+        userModel.setUserType(UserType.STUDENT);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userService.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+
+}
