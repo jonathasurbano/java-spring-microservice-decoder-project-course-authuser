f0ee831f6bafd6d9a863e149d72f5f8fefc72cd4
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-15 18:45:14 -0300

    add treatments for data validation and JsonView

--- src/main/java/com/onlinelearning/authuser/dtos/UserDto.java
+++ src/main/java/com/onlinelearning/authuser/dtos/UserDto.java
@@ -1,28 +1,65 @@
 package com.onlinelearning.authuser.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.onlinelearning.authuser.enums.UserStatus;
-import com.onlinelearning.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
 
+    public interface UserView {
+        interface RegistrationPost {
+        }
+
+        interface UserPut {
+        }
+
+        interface PasswordPut {
+        }
+
+        interface ImagePut {
+        }
+    }
+
     private UUID userId;
+
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
+    @JsonView(UserView.RegistrationPost.class)
     private String userName;
+
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min = 6, max = 20)
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 6, max = 20)
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+
+    @NotBlank(groups = UserView.ImagePut.class)
+    @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
 }
