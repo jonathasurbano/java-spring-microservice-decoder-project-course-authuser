3ebbb10b1cd5605fdfd041b97a6b8c61063e860d
Jonathas Urbano <jonathas.urbano@protonmail.com>
2023-05-15 18:27:57 -0300

    add annotations for Validation and JsonView

--- src/main/java/com/onlinelearning/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/onlinelearning/authuser/controllers/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.onlinelearning.authuser.controllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.onlinelearning.authuser.dtos.UserDto;
 import com.onlinelearning.authuser.enums.UserStatus;
 import com.onlinelearning.authuser.enums.UserType;
@@ -9,6 +10,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,7 +25,8 @@ public class AuthenticationController {
     UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
+                                               @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
         if (userService.existsByUserName(userDto.getUserName())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: UserName is already taken.");
         }
